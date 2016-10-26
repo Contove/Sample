cc754eb5a46703156388c05967fa919a38af1fc1
maborui <maborui001@gmail.com>
2016-10-26 06:55:56 +0000

    init repo

---
+++ test/controllers/staticpage_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class StaticpageControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get staticpage_home_url
+    assert_response :success
+  end
+
+  test "should get help" do
+    get staticpage_help_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get staticpage_about_url
+    assert_response :success
+  end
+
+end
