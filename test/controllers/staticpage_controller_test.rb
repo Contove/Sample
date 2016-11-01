b10cc9124d59194ddba21ea2f0843f95f1624f49
maborui <maborui001@gmail.com>
2016-11-01 16:45:36 +0000

    1/11/2016

--- test/controllers/staticpage_controller_test.rb
+++ test/controllers/staticpage_controller_test.rb
@@ -10,19 +10,19 @@ class StaticpageControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get staticpage_home_url
     assert_response :success
-    assert_select "title","Home"
+    assert_select "title","Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get staticpage_help_url
     assert_response :success
-    assert_select "title","Help"
+    assert_select "title","Help | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
     get staticpage_about_url
     assert_response :success
-    assert_select "title","About"
+    assert_select "title","About | Ruby on Rails Tutorial Sample App"
   end
 
 end
