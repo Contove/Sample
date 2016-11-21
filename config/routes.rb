abd1f372c5d635fda17c0fcfb132e12d61029574
maborui <maborui001@gmail.com>
2016-11-21 15:11:10 +0000

    21/11/2016

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,15 @@ Rails.application.routes.draw do
   
   
 
+  get 'users/new'
+
   root 'staticpage#home'
   
   root 'static_pages#home'
   get  '/help',    to: 'staticpage#help'
   get  '/about',   to: 'staticpage#about'
   get '/login',   to: 'staticpage#login'
+  get  '/signup',  to: 'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
