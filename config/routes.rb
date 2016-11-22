855d193b46b910bd20da00fc27f10998b41a25bb
maborui <maborui001@gmail.com>
2016-11-22 22:58:12 +0000

    22/11/2016

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   get  '/about',   to: 'staticpage#about'
   get '/login',   to: 'staticpage#login'
   get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
